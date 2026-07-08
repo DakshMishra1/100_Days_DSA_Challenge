84d1457f5c7839c3822ab5ec3b997dc580cf9828
Daksh Mishra <Shailym5777@gmail.com>
2026-07-09 00:02:39 +0530

    Solution for question Day_8

--- Day_8.java
+++ Day_8.java
@@ -14,3 +14,34 @@
 // 32
 
 // Explanation: 2^5 = 2 * 2 * 2 * 2 * 2 = 32
+
+// Solution 
+
+import java.util.*;
+
+public class Day_8{
+
+    public int pow(int a, int b){
+
+        if(b == 0){
+            return 1;
+        }
+        return a*pow(a, b-1);
+
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        Day_8 obj = new Day_8();
+        
+        System.out.print(obj.pow(a,b));
+        
+        sc.close();
+
+
+    }
+}
