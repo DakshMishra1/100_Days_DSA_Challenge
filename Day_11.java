55e82c8f33537ad16a47b832b2f818b24a652d24
Daksh Mishra <Shailym5777@gmail.com>
2026-07-10 01:23:57 +0530

    Solution Day_11

--- Day_11.java
+++ Day_11.java
@@ -63,3 +63,37 @@
 
 // Solution
 
+import java.util.*;
+
+public class Day_11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[m][n];
+        int[][] matrix2 = new int[m][n];
+
+        for(int i = 0; i < m; i++){
+            for( int j = 0; j < n; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < m; i++){
+            for( int j = 0; j < n; j++){
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < m; i++){
+            for( int j = 0; j < n; j++){
+                System.out.print(matrix[i][j] + matrix2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
