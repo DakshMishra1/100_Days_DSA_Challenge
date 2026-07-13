9a75548f57692034ac55d121a6f241b63f019b20
Daksh Mishra <Shailym5777@gmail.com>
2026-07-13 23:12:15 +0530

    First try

--- Day_14.java
+++ Day_14.java
@@ -1,4 +1,5 @@
-// Problem: Write a program to check whether a given square matrix is an Identity Matrix. An identity matrix is a square matrix in which all diagonal elements are 1 and all non-diagonal elements are 0.
+// Problem: Write a program to check whether a given square matrix is an Identity Matrix. An identity matrix is a square matrix in which all diagonal elements are 1 and all 
+// non-diagonal elements are 0.
 
 // Input:
 // - First line: integer n representing number of rows and columns
@@ -19,3 +20,31 @@
 // Identity Matrix
 
 // solution
+
+
+/*
+bool diagonal = false;
+bool non_dia = false;
+
+for(i = 0; i < m; i++){
+    for(j= 0; j <n; j++){
+        if (m == n){
+            if(arr[m][n] == 1){
+                diagonal = true;
+                return diagonal;
+            }
+        }
+        if(m != n){
+            if(arr[m][n] == 0){
+                non_dia = true;
+                return non_dia;
+            }
+        }
+    }    
+}
+
+if(diagonal && non_diagonal == true){
+    sys.out.print("Identity matrix");
+}
+
+*/
