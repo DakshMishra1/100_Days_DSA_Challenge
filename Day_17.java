e9bde92c44a322316afe3c97a53c863e7a1993b6
Daksh Mishra <Shailym5777@gmail.com>
2026-07-15 23:43:03 +0530

    Min and Max value in arr

--- Day_17.java
+++ Day_17.java
@@ -24,7 +24,8 @@ public class Day_17 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-
+        int n = sc.nextInt();
+        int[] arr = new int[n];
 
 
         sc.close();
