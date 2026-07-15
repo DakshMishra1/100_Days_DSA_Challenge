8dc115ee2c594d66c9ddd27f525d7b53f9b75766
Daksh Mishra <Shailym5777@gmail.com>
2026-07-15 23:34:25 +0530

    Min and Max value in arr

---
+++ Day_17.java
@@ -0,0 +1,23 @@
+// Problem: Write a program to find the maximum and minimum values present in a given array of integers.
+
+// Input:
+// - First line: integer n
+// - Second line: n integers
+
+// Output:
+// - Print the maximum and minimum elements
+
+// Example:
+// Input:
+// 6
+// 3 5 1 9 2 8
+
+// Output:
+// Max: 9
+// Min: 1
+
+//Solution
+
+public class Day_17 {
+    
+}
