f5a49d4510328ea57b5f77e7a3e09f3fc3d63876
Daksh Mishra <Shailym5777@gmail.com>
2026-07-16 23:26:22 +0530

    The reverse algo solution

--- Day_18.java
+++ Day_18.java
@@ -20,6 +20,45 @@
 
 // Solution
 
+import java.util.*;
 public class Day_18 {
-    
+
+    private static void reverse(int[] arr, int left , int right){
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+
+        }
+    }
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+
+        k = k % n;
+
+        reverse(arr , 0, n -1);
+        reverse(arr , 0, k -1);
+        reverse(arr , k , n-1);
+        
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i]);
+            if(i < n - 1) System.out.print(" ");
+        }
+        System.out.println();
+
+        sc.close();
+    }
 }
