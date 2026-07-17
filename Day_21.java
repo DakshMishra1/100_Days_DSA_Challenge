5b1d773098c21acb9ee1e12ecc59c1c52b5e4fec
Daksh Mishra <Shailym5777@gmail.com>
2026-07-17 22:47:04 +0530

    Question for Day_21

--- Day_21.java
+++ Day_21.java
@@ -17,6 +17,13 @@
 
 // Solution
 
+import java.util.*;
 public class Day_21 {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
+    }
 }
