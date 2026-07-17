6b17b4b2c22a6844795b7e1bae0d69d01c247779
Daksh Mishra <Shailym5777@gmail.com>
2026-07-17 22:44:27 +0530

    Question for Day_21

---
+++ Day_21.java
@@ -0,0 +1,22 @@
+// Problem: Create and Traverse Singly Linked List
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers
+
+// Output:
+// - Print the result
+
+// Example:
+// Input:
+// 5
+// 10 20 30 40 50
+
+// Output:
+// 10 20 30 40 50
+
+// Solution
+
+public class Day_21 {
+    
+}
