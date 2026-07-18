6de64766c1f5e625715f9b9b423ab2d222843c3b
Daksh Mishra <Shailym5777@gmail.com>
2026-07-18 23:41:20 +0530

    Solution Sum close to zero

--- Day_19.java
+++ Day_19.java
@@ -25,10 +25,39 @@ public class Day_19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
 
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
 
+        Arrays.sort(arr);
 
+        int left = 0;
+        int right = n - 1;
+
+        int minSum = Integer.MAX_VALUE;
+        int first = arr[left];
+        int second = arr[right];
+
+        while (left < right){
+            int sum = arr[left] + arr[right];
+
+            if(Math.abs(sum) < Math.abs(minSum)){
+                minSum = sum;
+                first = arr[left];
+                second = arr[right];
+            }
+
+            if (sum < 0 ) {
+                left++;                
+            }else{
+                right--;
+            }
+        }
+
+        System.out.println(first + " " + second);
 
         sc.close();
 
