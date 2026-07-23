5871369ad0eccb802a64e0472af54ca44d25596a
Daksh Mishra <Shailym5777@gmail.com>
2026-07-23 23:50:14 +0530

    Solution for Day_27

--- Day_27.java
+++ Day_27.java
@@ -39,7 +39,39 @@ public class Day_27 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        Node head = null;
+        Node tail = null;
+
+        for(int i = 0; i < n; i++){
+            int value = sc.nextInt();
+            Node newNode = new Node(value);
+
+            if(head == null){
+                head = newNode;
+                tail = newNode;
+            }else{
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
         
+        int m = sc.nextInt();
+        Node head2 = null;
+        Node tail2 = null;
+
+        for(int j = 0; j < m; j++){
+            int value = sc.nextInt();
+            Node newNode = new Node(value);
+
+            if(head2 == null){
+                head2 = newNode;
+                tail2 = newNode;
+            }else{
+                tail2.next = newNode;
+                tail2 = newNode;
+            }
+        }
 
 
 
