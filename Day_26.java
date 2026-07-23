fec5acd656d0c8645ac680f5128678f0f1a10232
Daksh Mishra <Shailym5777@gmail.com>
2026-07-23 23:12:29 +0530

    Soultion for Day_26

--- Day_26.java
+++ Day_26.java
@@ -20,6 +20,45 @@
 
 //Solution
 
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int value){
+        this.data = value;
+        this.next = null;
+    }
+}
+
 public class Day_26 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Node head = null;
+        Node tail = null;
+
+        for(int i = 0; i < n; i++){
+            int value = sc.nextInt();
+            Node newNode = new Node(value);
+
+            if(head == null){
+                head = newNode;
+                tail = newNode;
+            }else{
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+
+
+
+
+
+        sc.close();
+    }   
 }
