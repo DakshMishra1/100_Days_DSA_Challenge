8190a466b2657fe387c49e36513e6ead66c6e516
Daksh Mishra <Shailym5777@gmail.com>
2026-07-26 23:40:07 +0530

    Circular LL creation and Traversal

--- Day_28.java
+++ Day_28.java
@@ -20,3 +20,51 @@
 
 // Solution
 
+import java.util.Scanner;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int value){
+        this.data = value;
+        this.next = null;
+    }
+}
+public class Day_28{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Node head = null;
+        Node tail = null;
+        
+        for(int i = 0; i < n; i++){
+            int value = sc.nextInt();
+            Node newNode = new Node(value);
+
+            if(head == null){
+                head = newNode;
+                tail = newNode;
+            }else{
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        if(tail != null){
+            tail.next = head;
+        }
+
+        if(head != null){
+            Node curr = head;
+
+            do { 
+                System.out.print(curr.data + " ");
+                curr = curr.next; 
+            } while (curr != head);
+        }
+
+        sc.close();
+    }
+}
